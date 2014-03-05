e3b842a2fbf4cec47aaeead8908df9c6c3ef0ce8
Stephen Parker <shurakaisoft@gmail.com>
2014-03-05 16:38:22 +1100

    Added Database and RESTful API for todos

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var mongoose = require('mongoose'),
 
 // variables
 var server = express(),
-	port = 8080;
+	port = 8080,
+    Todo;
 
+// server setup
+mongoose.connect('mongodb://localhost/todolist');
 server.configure(function () {
 	server.use(express.static(__dirname + '/public'));
 	server.use(express.logger('dev'));
@@ -18,6 +21,68 @@ server.configure(function () {
 });
 
 
+// Model for todo list
+Todo = mongoose.model('Todo', {
+    text: String
+});
+
+
+// RESTful API routes
+
+// helper function
+function sendAllTodos(res) {
+    Todo.find({}, function (err, todos) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(todos);
+        }
+    });
+}
+
+
+/**
+ * Get all of the todos.
+ */
+server.get('/api/todos', function (req, res) {
+    sendAllTodos(res);
+});
+
+
+/**
+ * Create a single todo
+ */
+server.post('api/todos', function (req, res) {
+    Todo.create({
+        text: req.body.text,
+        done: false
+    }, function (err, todo) {
+        if (err) {
+            res.send(err);
+        } else {
+            sendAllTodos(res);
+        }
+    });
+});
+
+
+/**
+ * Delete a single todo
+ */
+server.delete('api/todos/:todoId', function (req, res) {
+    Todo.remove({
+        _id: req.params.todoId
+    }, function (err, todo) {
+        if (err) {
+            res.send(err);
+        } else {
+            sendAllTodos(res);
+        }
+    });
+});
+
+
+// start the server
 server.listen(port, function (err) {
 	if (err) {
 		console.log("error:", err);
@@ -25,3 +90,4 @@ server.listen(port, function (err) {
 		console.log("Server listening on port", port);
 	}
 });
+
